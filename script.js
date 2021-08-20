b434f7ad03962cbcab96b9ba55210e475212c121
dulkifal <71825494+dulkifal@users.noreply.github.com>
2021-08-20 22:09:32 +0530

    submit

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+$("#submit-form").submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+        url:"https://script.google.com/macros/s/AKfycbweIFSA6Z2u9F9n4eWKVWSIBj2TgUc9IMQ5JGS-UXiL54NtiFKh0uhH3a_6mZVW2K6O/exec",
+        data:$("#submit-form").serialize(),
+        method:"post",
+        success:function (response){
+            alert("Form submitted successfully")
+            window.location.reload()
+            // window.location.href="https://google.com"
+        },
+        error:function (err){
+            alert("Something Error")
+
+        }
+    })
+})
+
 window.addEventListener('scroll', function(){
 const header = this.document.querySelector("header");
 header.classList.toggle('sticky', window.scrollY >0);
